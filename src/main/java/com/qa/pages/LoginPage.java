8bdee495061b2b17e536d6c1f7a6874dbbdbd218
deepak kumar <agdeepak.singh@gmail.com>
2025-07-06 17:08:23 +0530

    added search page Feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,4 +5,8 @@ public class LoginPage {
     public void loginPage(){
         System.out.println("login page");
     }
+
+    public void loginSet(){
+        System.out.println("login set");
+    }
 }
